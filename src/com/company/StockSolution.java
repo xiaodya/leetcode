d1c1b37f1b18e45b1a57e02937327f213eacd7d7
xiaodya <xiaodya@163.com>
2021-06-07 13:27:43 +0800

    1.补充股票买卖系列，1次，2次，多次的dp，greedy算法

--- src/com/company/StockSolution.java
+++ src/com/company/StockSolution.java
@@ -39,14 +39,89 @@ public class StockSolution {
         return sell3;
     }
 
+    /**
+     * 1次
+     * @param prices
+     * @return
+     */
+    public int maxProfitOneDP(int[] prices){
+        int[][] profit = new int[prices.length][3];
+        int res = 0;
+        for (int i=0;i<prices.length;i++){
+            if (i==0){
+                profit[i][0] = 0;           //状态位，0没有股票
+                profit[i][1] = -prices[i];  //1手里有股票
+                profit[i][2] = 0;           //1手里有股票
+            }else{
+                profit[i][0] = profit[i-1][0];
+                profit[i][1] = Math.max(profit[i-1][1],profit[i-1][0]-prices[i]);
+                profit[i][2] = profit[i-1][1]+prices[i];
+            }
+            res = Math.max(res,Math.max(profit[i][0],profit[i][2]));
+        }
+        return res;
+    }
+
+    /**
+     * 无限次
+     * @param prices
+     * @return
+     */
+    public int maxProfitUnLimitDP(int[] prices){
+        int[][] profit = new int[prices.length][2];
+        int res = 0;
+        for (int i=0;i<prices.length;i++){
+            if (i==0){
+                profit[i][0] = 0;           //状态位，0没有股票
+                profit[i][1] = -prices[i];  //1手里有股票
+            }else{
+                profit[i][0] = Math.max(profit[i-1][1]+prices[i],profit[i-1][0]);
+                profit[i][1] = Math.max(profit[i-1][1],profit[i-1][0]-prices[i]);
+            }
+            res = Math.max(res,Math.max(profit[i][0],profit[i][1]));
+        }
+        return res;
+    }
+
+    /**
+     * 无限次，贪心
+     */
+    public int maxProfitUnLimitGreedy(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                profit += prices[i + 1 ] - prices[i];
+            }
+        }
+        return profit;
+    }
+
+    /**
+     * 泛型dp
+     * @param k
+     * @param prices
+     * @return
+     */
+    public int maxProfitK(int k, int[] prices){
+        return 0;
+    }
+
     public static void main(String args[]){
-        int[] prices = new int[]{2,4,6,1,7,3,8};
+        int[] prices = new int[]{2,4,6,1,7,3,8,5,9};
         StockSolution stockSolution = new StockSolution();
         int one = stockSolution.maxProfitOne(prices);
         int two = stockSolution.maxProfitTwo(prices);
         int three = stockSolution.maxProfitThree(prices);
-        System.out.println(one);
-        System.out.println(two);
-        System.out.println(three);
+        System.out.println("one="+one);
+        System.out.println("two="+two);
+        System.out.println("three="+three);
+
+        int oneDp = stockSolution.maxProfitOneDP(prices);
+        int unLimitDp = stockSolution.maxProfitUnLimitDP(prices);
+        int unLimitGreedy = stockSolution.maxProfitUnLimitGreedy(prices);
+        System.out.println("oneDp="+oneDp);
+        System.out.println("unLimitDp="+unLimitDp);
+        System.out.println("unLimitGreedy="+unLimitGreedy);
+
     }
 }
